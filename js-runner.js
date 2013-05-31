f01dbb451e4a5d044a39d4575177f29a603ae95e
Gideon Goldberg <gidsgoldberg@gmail.com>
2013-05-31 17:09:02 +0100

    updated runner and added package.json

--- js-runner.js
+++ js-runner.js
@@ -1,6 +1,15 @@
 var expect = require('chai').expect;
 var webdriverjs = require("webdriverjs");
-var client = webdriverjs.remote();
+var client = webdriverjs.remote({
+    desiredCapabilities: {
+        // You may choose other browsers
+        // http://code.google.com/p/selenium/wiki/DesiredCapabilities
+        browserName: 'phantomjs'
+    },
+    // webdriverjs has a lot of output which is generally useless
+    // However, if anything goes wrong, remove this to see more details
+    //logLevel: 'silent'
+});
 
 client.logLevel = "verbose";
 
