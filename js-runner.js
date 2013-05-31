d0d7b698e8cfc56cac2b90f0cc2f3c983495124e
meza <meza@meza.hu>
2013-05-31 16:12:34 +0100

    fixed runner

--- js-runner.js
+++ js-runner.js
@@ -1,21 +1,25 @@
-var expect = require('chai').expect,
-    webdriverjs = require("webdriverjs"),
-    client = webdriverjs.remote();
+var expect = require('chai').expect;
+var webdriverjs = require("webdriverjs");
+var client = webdriverjs.remote();
 
-describe('Run Selenium tests', function() {
+client.logLevel = "verbose";
 
-    before(function(done) {
+describe('Run Selenium tests', function () {
+
+    before(function (done) {
         // Add some helper commands
-        client.addCommand('hasText', function(selector, text, callback) {
-            this.getText(selector, function(error, result) {
+        client.addCommand('hasText', function (selector, text, callback) {
+            this.getText(selector, function (error, result) {
                 expect(result).to.have.string(text);
                 callback();
             });
         });
-        client.addCommand('waitUntilVisible', function(element, callback) {
+
+        client.addCommand('waitUntilVisible', function (element, callback) {
             var self = this;
+
             function checkElement() {
-                self.isVisible(element, function(error, result) {
+                self.isVisible(element, function (error, result) {
                     if (result === true) {
                         callback();
                     } else {
@@ -23,29 +27,31 @@ describe('Run Selenium tests', function() {
                     }
                 });
             }
+
             checkElement();
         });
         done();
     });
 
-    beforeEach(function(done) {
+    beforeEach(function (done) {
         // Navigate to the URL for each test
         client.init();
         client.url('http://m.gucode.co.uk/sport/2012/oct/10/icc-suspends-umpires-corruption-claims', done);
     });
-    
-    it('should be able to navigate betwen the pages', function(done) {
+
+    it('should be able to navigate betwen the pages', function (done) {
         this.timeout(10000);
-        client.waitUntilVisible('aside h3', function(){
-           
-            this.hasText('aside h3', 'More on this story')
-            done();     
+
+        client.waitUntilVisible('aside h3', function () {
+            this.hasText('aside h3', 'More on this story', function () {
+                done()
+            })
         })
-   
-            
+
+
     });
-        
-    afterEach(function(done) {
+
+    afterEach(function (done) {
         client.end();
         done();
     });
