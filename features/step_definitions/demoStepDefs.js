13e2d7c5153631f8d5847c29a17255632c783677
meza <meza@meza.hu>
2013-06-03 16:38:10 +0100

    removed gucode

--- features/step_definitions/demoStepDefs.js
+++ features/step_definitions/demoStepDefs.js
@@ -2,7 +2,7 @@ var stepDefs = function () {
     this.World = require("../support/world.js").World;
 
     this.When(/^I open an article$/, function (callback) {
-        this.visit("http://m.gucode.co.uk/sport/2012/oct/10/icc-suspends-umpires-corruption-claims", callback);
+        this.visit("http://m.guardian.co.uk/sport/2012/oct/10/icc-suspends-umpires-corruption-claims", callback);
     });
 
     this.Then(/^I should see an option to read more on the story$/, function (callback) {
